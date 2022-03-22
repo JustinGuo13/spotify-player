d20490f185f0d76717e5aee1128f579703c55c31
Peng Yang Guo <pyangguo@gmail.com>
2022-03-22 15:14:43 -0700

    Finish app

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -28,31 +28,31 @@ function Sidebar() {
 	}, [session, spotifyApi]);
 
 	return (
-		<div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem]hidden md:inline-flex">
+		<div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem]hidden md:inline-flex pb-36">
 			<div className="space-y-4">
 				<button className="flex items-center space-x-2 hover:text-white">
-					<HomeIcon className="h-5 w-5" />
+					<HomeIcon className="h-5 w-5 text-orange-500" />
 					<p>Home</p>
 				</button>
 				<button className="flex items-center space-x-2 hover:text-white">
-					<SearchIcon className="h-5 w-5" />
+					<SearchIcon className="h-5 w-5 text-yellow-500" />
 					<p>Search</p>
 				</button>
 				<button className="flex items-center space-x-2 hover:text-white">
-					<LibraryIcon className="h-5 w-5" />
+					<LibraryIcon className="h-5 w-5 text-purple-500	" />
 					<p>Your Library</p>
 				</button>
 				<hr className="border-t-[1px] border-gray-900" />
 				<button className="flex items-center space-x-2 hover:text-white">
-					<PlusCircleIcon className="h-5 w-5" />
+					<PlusCircleIcon className="h-5 w-5 text-blue-500" />
 					<p>Create Playlist</p>
 				</button>
 				<button className="flex items-center space-x-2 hover:text-white">
-					<HeartIcon className="h-5 w-5" />
+					<HeartIcon className="h-5 w-5 text-pink-500" />
 					<p>Liked songs</p>
 				</button>
 				<button className="flex items-center space-x-2 hover:text-white">
-					<RssIcon className="h-5 w-5" />
+					<RssIcon className="h-5 w-5 text-green-500" />
 					<p>Your Episodes</p>
 				</button>
 				<hr className="border-t-[1px] border-gray-900" />
